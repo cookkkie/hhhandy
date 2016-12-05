a07638900c6c9c381634466ae0d9d4fcae31feac
cookkkie <cookie.io@openmailbox.org>
2016-12-05 04:18:10 +0100

    Add config.py

---
+++ hhhandy/config.py
@@ -0,0 +1,15 @@
+import re
+import os
+
+class Config(object):
+    @classmethod
+    def from_env(cls):
+        config = cls
+        config_var_pattern = r'^[A-Z_]*$'
+        attrs = filter(lambda v: re.match(config_var_pattern, v))
+        for attr in attrs:
+            value = os.getenv(attr)
+            if value:
+                setattr(config, attr, value)
+
+        return config
